35023d876d25f8cb4b9f060856f804d43c2213d4
William Franks <136557841+wfranksadobe@users.noreply.github.com>
2023-11-03 16:02:00 +1100

    Update cfoffer.js
    updated publish url

--- blocks/cfoffer/cfoffer.js
+++ blocks/cfoffer/cfoffer.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
    
-    const aempublishurl = 'https://publish-p55117-e571178.adobeaemcloud.com'
+    const aempublishurl = 'https://publish-p107502-e1212957.adobeaemcloud.com'
     const persistedquery = '/graphql/execute.json/securbank/OfferByPath'
     const cfpath = '/content/dam/securbank/en/offers/'
     
